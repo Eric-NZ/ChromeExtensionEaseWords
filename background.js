dcaeeb260dcca75750c770578a7944e1403bef5a
Eric Ho <lomocode@Lomobook.local>
2018-10-08 10:32:37 +1300

    contextMenu item appears with sub menus as unexpected

--- background.js
+++ background.js
@@ -1,8 +1,30 @@
-var selectedWord = "none selected"
+
 // handle received message
 chrome.runtime.onMessage.addListener(onReceiveMessage)
 
 function onReceiveMessage(request, sender, sendResponse) {
-    selectedWord = request.msgText
-    console.log(selectedWord)
+    let selectedWord = request.msgText
+    let isContextMenu = request.isContextMenu
+
+    // create context menu item
+    if (isContextMenu) {
+        createContextMenu(selectedWord)
+    }
+}
+
+// create a context menu item with selected text
+// BUT, NOTE: chrome.contextMenus is undefined in the content script.
+function createContextMenu(selectedText) {
+    // menu item: object
+    let item = {
+        title: selectedText,
+        contexts: ['selection'],
+        onclick: onMenuItemClicked
+    };
+    chrome.contextMenus.create(item);
+    console.log(item)
+}
+
+function onMenuItemClicked() {
+    console.log("Menu item clicked!")
 }
--- content.js
+++ content.js
@@ -1,28 +1,35 @@
 
+// either context menu or not
+var isContextMenu = false;
 // selected text: using setter of JavaScript
 var selectedText =  {
     set currentValue(newValue) {
-        console.log(newValue);
+        // create a context menu item
+        // NOTE: chrome.contextMenus is undefined in the content script.
+        // So, create context menu item in background instead. 
+        // createContextMenu(newValue)
+
+        // message is a json object
+        let message = {
+            "msgText": newValue,
+            "isContextMenu": isContextMenu
+        };
+        // send a message with the selected text to the background page
+        chrome.runtime.sendMessage(message);
     }
 }
 
 // Add a listener on double click or click event
 window.addEventListener('mouseup', onMouseUp);
 // Add a listener on right click event
-window.addEventListener('contextmenu', onContentMenu)
+window.addEventListener('contextmenu', onContentMenu);
 
 // handle mouse up event
 function onMouseUp() {
-    let text = window.getSelection().toString()
+    let text = window.getSelection().toString();
     if (text.length > 0) {
-        // message is a json object
-        let message = {
-            "msgText": text
-        };
-        // send a message with the selected text to the background page
-        // chrome.runtime.sendMessage(message)
-
-        selectedText.currentValue = text
+        selectedText.currentValue = text;
+        isContextMenu = false;
     }
 }
 
@@ -31,7 +38,7 @@ function onContentMenu() {
     // get selected text
     let text = window.getSelection().toString()
     if (text.length > 0) {
-        selectedText.currentValue = text
+        selectedText.currentValue = text;
+        isContextMenu = true;
     }
-    
 }
